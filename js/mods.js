92f1ad1af571d75aac236b2fa304ecf34a68a87a
Neil Gershenfeld <gersh@cba.mit.edu>
2017-06-15 17:13:39 -0400

    test

--- js/mods.js
+++ js/mods.js
@@ -128,7 +128,7 @@ var sel = document.createElement('select')
       evt.target.value = 'programs'
       })
    var opt = document.createElement('option')
-      opt.text = 'programs test'
+      opt.text = 'programs'
       opt.value = opt.text
       sel.add(opt)
    var opt = document.createElement('option')
