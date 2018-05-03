8f1840fba64e4d3d882ba0c6d668f856d9c9a5be
Neil Gershenfeld <gersh@cba.mit.edu>
2018-05-02 21:44:27 -0400

    add involute sample

--- programs/index.js
+++ programs/index.js
@@ -1,5 +1,6 @@
 program_label('frep')
 program_menu('   cuboct','programs/frep/cuboct')
+program_menu('   gears','programs/frep/gears')
 program_label('image')
 program_menu('   function','programs/image/function')
 program_menu('   motion detect','programs/image/motion%20detect')
